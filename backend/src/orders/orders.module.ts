0c7a4d217af664b27cb69d1f3bb57418f89ce552
sayandhar23-source <sayandhar23@gmail.com>
2026-08-01 12:51:56 +0530

    Update orders.module.ts

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -5,12 +5,14 @@ import { OrdersController } from './orders.controller';
 import { Order, OrderSchema } from '../common/schemas/order.schema';
 import { MailModule } from '../mail/mail.module';
 import { ReferralsModule } from '../referrals/referrals.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
     MailModule,
     ReferralsModule,
+    NotificationsModule,
   ],
   controllers: [OrdersController],
   providers: [OrdersService],
