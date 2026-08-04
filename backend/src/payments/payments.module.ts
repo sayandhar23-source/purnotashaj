5d95c3f632b9cfe0212f525b9964c7c59b950c4a
sayandhar23-source <sayandhar23@gmail.com>
2026-08-04 06:41:00 +0530

    Update payments.module.ts

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -4,11 +4,13 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { Order, OrderSchema } from '../common/schemas/order.schema';
 import { MailModule } from '../mail/mail.module';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
     MailModule,
+    ChatModule,
   ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
